200a58d0405a74deb6ba9ab2ab042191f6cf70c9
KevinH <khui98@gmail.com>
2025-10-18 15:27:44 -0400

    Update App.jsx

--- 42_Local_React_Apps_With_Vite/dice-game/src/App.jsx
+++ 42_Local_React_Apps_With_Vite/dice-game/src/App.jsx
@@ -3,13 +3,18 @@ import Lucky7 from "./Lucky7";
 import Die from "./Die";
 import Dice from "./Dice";
 import LuckyN from "./LuckyN";
+import { sum } from "./Utils.js";
+
+function lessThanFour(dice) {
+  return sum(dice) < 4;
+}
 
 function App() {
   return (
     <>
       <h1>Dice Game</h1>
-      <LuckyN numDice={4} goal={21} />
-      <LuckyN numDice={4} goal={21} />
+      <LuckyN numDice={3} winCheck={10} />
+      <LuckyN numDice={3} winCheck={lessThanFour} />
       {/* <Dice dice={[3, 6, 1]} />
       <Dice dice={[3, 6, 1]} color="red" />
       <Dice dice={[3, 6, 1]} color="blue" /> */}
