280df1e9e6961ada08a0a8dc190dd688ee01c072
KevinH <khui98@gmail.com>
2025-10-18 15:27:41 -0400

    Update LuckyN.jsx

--- 42_Local_React_Apps_With_Vite/dice-game/src/LuckyN.jsx
+++ 42_Local_React_Apps_With_Vite/dice-game/src/LuckyN.jsx
@@ -3,12 +3,12 @@ import { getRolls, sum } from "./Utils";
 import Dice from "./Dice";
 import "./Button.css";
 
-export default function LuckyN({ numDice = 2, goal = 7 }) {
+export default function LuckyN({ numDice = 2, winCheck = 7 }) {
   // Creates a state for dice value
   const [dice, setDice] = useState(getRolls(numDice));
 
   // Winning dice sum is equal to target
-  const isWinner = sum(dice) === goal;
+  const isWinner = sum(dice) === winCheck;
 
   // Re-rolls the dice
   const roll = () => {
@@ -18,7 +18,7 @@ export default function LuckyN({ numDice = 2, goal = 7 }) {
   return (
     <main className="LuckyN">
       <h1>
-        Lucky{goal} {isWinner && "You Win!"}
+        Lucky{winCheck} {isWinner && "You Win!"}
       </h1>
       <Dice dice={dice} />
       <button onClick={roll} className="Button">
