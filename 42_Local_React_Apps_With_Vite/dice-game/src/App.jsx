17fc361afee81a89b6b19264b8fbd2131904cc31
KevinH <khui98@gmail.com>
2025-10-19 16:30:19 -0400

    Update App.jsx

--- 42_Local_React_Apps_With_Vite/dice-game/src/App.jsx
+++ 42_Local_React_Apps_With_Vite/dice-game/src/App.jsx
@@ -3,26 +3,30 @@ import Lucky7 from "./Lucky7";
 import Die from "./Die";
 import Dice from "./Dice";
 import LuckyN from "./LuckyN";
-import { sum } from "./Utils.js";
+// import { sum } from "./Utils.js";
+import Box from "./Box.jsx";
+import BoxGrid from "./BoxGrid.jsx";
 
 // Function to pass as a prop for winCheck in LuckyN element
-function lessThanFour(dice) {
-  return sum(dice) < 4;
-}
+// function lessThanFour(dice) {
+//   return sum(dice) < 4;
+// }
 
-function allSame(dice) {
-  return dice.every((v) => v === dice[0]);
-}
+// function allSame(dice) {
+//   return dice.every((v) => v === dice[0]);
+// }
 
 function App() {
   return (
     <>
-      <h1>Dice Games</h1>
-      <h2>Less Than Four</h2>
-      {/* lessThanFour function is referenced, not executed */}
+      <BoxGrid numBox={15} />
+
+      {/* <h1>Dice Games</h1>
+      <h2>Roll Less Than Four</h2>
       <LuckyN numDice={2} winCheck={lessThanFour} />
-      <h2>Same Number</h2>
-      <LuckyN numDice={2} winCheck={allSame} />
+      <h2>Roll Same Numbers</h2>
+      <LuckyN numDice={2} winCheck={allSame} /> */}
+
       {/* <LuckyN numDice={2} winCheck={10} /> */}
       {/* <Dice dice={[3, 6, 1]} />
       <Dice dice={[3, 6, 1]} color="red" />
