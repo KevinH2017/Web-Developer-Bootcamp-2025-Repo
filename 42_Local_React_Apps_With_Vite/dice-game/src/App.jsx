f9201598373461379671ecc794137842f1b693b2
KevinH <khui98@gmail.com>
2025-10-19 11:21:42 -0400

    Update App.jsx

--- 42_Local_React_Apps_With_Vite/dice-game/src/App.jsx
+++ 42_Local_React_Apps_With_Vite/dice-game/src/App.jsx
@@ -5,16 +5,25 @@ import Dice from "./Dice";
 import LuckyN from "./LuckyN";
 import { sum } from "./Utils.js";
 
+// Function to pass as a prop for winCheck in LuckyN element
 function lessThanFour(dice) {
   return sum(dice) < 4;
+}
+
+function allSame(dice) {
+  return dice.every((v) => v === dice[0]);
 }
 
 function App() {
   return (
     <>
-      <h1>Dice Game</h1>
-      <LuckyN numDice={3} winCheck={10} />
-      <LuckyN numDice={3} winCheck={lessThanFour} />
+      <h1>Dice Games</h1>
+      <h2>Less Than Four</h2>
+      {/* lessThanFour function is referenced, not executed */}
+      <LuckyN numDice={2} winCheck={lessThanFour} />
+      <h2>Same Number</h2>
+      <LuckyN numDice={2} winCheck={allSame} />
+      {/* <LuckyN numDice={2} winCheck={10} /> */}
       {/* <Dice dice={[3, 6, 1]} />
       <Dice dice={[3, 6, 1]} color="red" />
       <Dice dice={[3, 6, 1]} color="blue" /> */}
