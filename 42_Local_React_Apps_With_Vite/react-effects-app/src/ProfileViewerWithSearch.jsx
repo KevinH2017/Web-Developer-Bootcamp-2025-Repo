0f35e329ffa947b74bc76c5403d6da2d94bd4238
KevinH <khui98@gmail.com>
2025-10-25 14:22:25 -0400

    Create ProfileViewerWithSearch.jsx

---
+++ 42_Local_React_Apps_With_Vite/react-effects-app/src/ProfileViewerWithSearch.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import ProfileSearchForm from "./ProfileSearchForm";
+import axios from "axios";
+
+// Shows GitHub profile information from GH API
+const BASE_URL = "https://api.github.com/users";
+
+function ProfileViewerWithSearch() {
+  const [username, setUsername] = useState("");
+  const [profile, setProfile] = useState({ data: null, isLoading: true });
+
+  useEffect(
+    function fetchUserOnUsernameChange() {
+      async function fetchUser() {
+        // Gets username from BASE_URL and github username
+        const userResult = await axios.get(`${BASE_URL}/${username}`);
+        setProfile({ data: userResult.data, isLoading: false });
+      }
+      fetchUser();
+    },
+    [username]
+  );
+
+  function search(username) {
+    setProfile({ data: null, isLoading: true });
+    setUsername(username);
+  }
+
+  if (profile.isLoading) return <i>Loading...</i>;
+
+  return (
+    <div>
+      <ProfileSearchForm search={search} />
+      <b>{profile.data.name}</b>
+      <img src={profile.data.avatar_url} />
+    </div>
+  );
+}
+
+export default ProfileViewerWithSearch;
